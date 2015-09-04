472a59fed4104154c9b49b5f66d1f642ee513a5e
Coen Zimmerman <coen@pixelindustries.com>
2015-09-04 11:57:05 +0200

    removed unnecessary dependency on translatable

--- tests/Helpers/TestSimpleModel.php
+++ tests/Helpers/TestSimpleModel.php
@@ -1,23 +1,16 @@
 <?php
 namespace Czim\Filter\Test\Helpers;
 
-use Dimsav\Translatable\Translatable;
 use Illuminate\Database\Eloquent\Model;
 
 class TestSimpleModel extends Model
 {
-    use Translatable;
-
     protected $fillable = [
         'unique_field',
         'second_field',
         'test_related_model_id',
         'name',
         'active',
-    ];
-
-    protected $translatedAttributes = [
-        'translated_string',
     ];
 
     public function RelatedModel()
