930f4bc6d84bb4618251acfb9ded9fc39ce958e5
Coen Zimmerman <coen@pixelindustries.com>
2016-05-01 12:19:31 +0200

    added empty and nonempty simple parameter filters

---
+++ src/ParameterFilters/NotEmpty.php
@@ -0,0 +1,48 @@
+<?php
+namespace Czim\Filter\ParameterFilters;
+
+use Czim\Filter\Contracts\FilterInterface;
+use Czim\Filter\Contracts\ParameterFilterInterface;
+use Illuminate\Database\Eloquent\Builder as EloquentBuilder;
+
+class NotEmpty implements ParameterFilterInterface
+{
+
+    /**
+     * @var null
+     */
+    protected $table;
+
+    /**
+     * @var null
+     */
+    protected $column;
+
+    /**
+     * @param string $table
+     * @param string $column   if given, overrules the attribute name
+     */
+    public function __construct($table = null, $column = null)
+    {
+        $this->table  = $table;
+        $this->column = $column;
+    }
+
+    /**
+     * Applies parameter filtering for a given query
+     *
+     * @param string          $name
+     * @param mixed           $value
+     * @param EloquentBuilder $query
+     * @param FilterInterface $filter
+     * @return EloquentBuilder
+     */
+    public function apply($name, $value, $query, FilterInterface $filter)
+    {
+        $column = ( ! empty($this->table) ? $this->table . '.' : null)
+                . ( ! empty($this->column) ? $this->column : $name);
+
+        return $query->whereNotNull($column)
+                     ->where($column, '!=', '');
+    }
+}
